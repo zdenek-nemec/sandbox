bb89c7b86d57ecabd9c0cd6a4cdaa16db4f31220
Nemec,Zdenek <Zdenek.Nemec@drei.com>
2018-06-19 12:03:15 +0200

    Python Udemy: Sandbox

--- python-udemy/sandbox.py
+++ python-udemy/sandbox.py
@@ -1,5 +1,11 @@
 def main():
     print("Hello World!")
+    my_list = [1, 2, 3, 4]
+    print(my_list)
+    print(my_list[1:3])
+    print(my_list[-3:-1])
+    print("Python is fun"[-3:][-1])
+    print(dir(my_list))
 
 
 if __name__ == '__main__':
