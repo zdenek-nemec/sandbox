98d48271358ddd7ead3eaa1ed63db4c18e739803
Nemec,Zdenek <Zdenek.Nemec@drei.com>
2018-06-19 07:59:59 +0200

    Python Udemy: Demos

--- python-udemy/sandbox.py
+++ python-udemy/sandbox.py
@@ -1,15 +1,5 @@
-class Demos(object):
-    def demo_type(self):
-        print(type(1))
-        print(type(1.0))
-        print(type(''))
-        print(type(()))
-        print(type([]))
-        print(type({}))
-
-
 def main():
-    Demos().demo_type()
+    print("Hello World!")
 
 
 if __name__ == '__main__':
