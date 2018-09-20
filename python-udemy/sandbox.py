3abbd8299bb49a31db2363aebd8165b3cf94c094
zdenek-nemec <zdenek.nemec@artin.cz>
2018-09-20 22:06:47 +0200

    Python Udemy: Quotes

--- python-udemy/sandbox.py
+++ python-udemy/sandbox.py
@@ -1,10 +1,10 @@
 def main():
-    print("Hello World!")
+    print('Hello, World!')
     my_list = [1, 2, 3, 4]
     print(my_list)
     print(my_list[1:3])
     print(my_list[-3:-1])
-    print("Python is fun"[-3:][-1])
+    print('Python is fun'[-3:][-1])
     print(dir(my_list))
 
 
