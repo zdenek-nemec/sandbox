dce930484a0abb57e356a0658406180b14c176ee
zdenek-nemec <zdenek.nemec@artin.cz>
2018-09-21 15:05:23 +0200

    Brain teaser 1

---
+++ brain-teasers/brain_teaser_1.py
@@ -0,0 +1,22 @@
+def main():
+    a = 2
+    b = 3
+
+    print(a - (-b))
+
+    value = 0
+    for i in range(0, abs(a)):
+        if (a > 0):
+            value -= -1
+        else:
+            value -= 1
+    for i in range(0, abs(b)):
+        if (b > 0):
+            value -= -1
+        else:
+            value -= 1
+    print(value)
+
+
+if __name__ == '__main__':
+    main()
