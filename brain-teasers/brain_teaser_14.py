7687a9e86cbf78be0a473881fdf792d31a228f2c
zdenek-nemec <zdenek.nemec@artin.cz>
2018-09-24 21:37:22 +0200

    Brain teaser 12

---
+++ brain-teasers/brain_teaser_14.py
@@ -0,0 +1,12 @@
+# Set both elements of a binary array to 0 in single line
+
+
+def main():
+    array = [1, 2]
+
+    array[0] = array[1] = 0
+    print(array)
+
+
+if __name__ == '__main__':
+    main()
