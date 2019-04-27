427c0a284c49e7b1a40624637e5fe61c52dfef48
zdenek-nemec <zdenek.nemec@artin.cz>
2019-04-27 15:08:47 +0200

    #5: 10. Numbers - Exercice 1 - Variables

---
+++ python-udemy/section02_python_basics/practice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def execise_1():
+    """Variables"""
+    name = "John Snow"
+    age = 29
+
+
+
+def main():
+    execise_1()
+
+
+if __name__ == "__main__":
+    main()
