da2263fd45199c6ff9e2d6bb098587d4c044be26
zdenek-nemec <zdenek.nemec@artin.cz>
2019-04-27 15:10:47 +0200

    #5:  10. Numbers - Exercice 2 - Get Variable Value

--- python-udemy/section02_python_basics/practice.py
+++ python-udemy/section02_python_basics/practice.py
@@ -7,9 +7,15 @@ def execise_1():
     age = 29
 
 
+def execice_2():
+    """Get Variable Value"""
+    price = 10
+    print(price)
+
 
 def main():
     execise_1()
+    execice_2()
 
 
 if __name__ == "__main__":
