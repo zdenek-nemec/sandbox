0315b6f9b40893e4a483d637dc5caa2a3a287ef9
zdenek-nemec <zdenek.nemec@artin.cz>
2019-04-27 15:15:15 +0200

    #5:  12. Math Operators - Exercice 3 - Math Operators

--- python-udemy/section02_python_basics/practice.py
+++ python-udemy/section02_python_basics/practice.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
 
-def execise_1():
+def exercise_1():
     """Variables"""
     name = "John Snow"
     age = 29
 
 
-def execice_2():
+def exercice_2():
     """Get Variable Value"""
     price = 10
     print(price)
 
 
+def exercice_3():
+    """Math Operators"""
+    x = 1
+    y = 2
+    z = 3
+    print(((x * y) ** z) / 8)
+
+
+
 def main():
-    execise_1()
-    execice_2()
+    exercise_1()
+    exercice_2()
+    exercice_3()
 
 
 if __name__ == "__main__":
