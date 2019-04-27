0b77462f8d03290e91227ed346343a6f0ce77f25
zdenek-nemec <zdenek.nemec@artin.cz>
2019-04-27 18:13:43 +0200

    #5: Fixed typos

--- python-udemy/section02_python_basics/practice.py
+++ python-udemy/section02_python_basics/practice.py
@@ -7,13 +7,13 @@ def exercise_1():
     age = 29
 
 
-def exercice_2():
+def exercise_2():
     """Get Variable Value"""
     price = 10
     print(price)
 
 
-def exercice_3():
+def exercise_3():
     """Math Operators"""
     x = 1
     y = 2
@@ -21,43 +21,43 @@ def exercice_3():
     print(((x * y) ** z) / 8)
 
 
-def exercice_4():
+def exercise_4():
     """Simple Sum"""
     a = 1.0
     b = 2
     print(int(a + b))
 
 
-def exercice_5():
+def exercise_5():
     """Lists"""
     mylist = [1, 2, 3]
 
 
-def exercice_6():
+def exercise_6():
     """Indexing"""
     name = "John"
     print(name[2])
 
 
-def exercice_7():
+def exercise_7():
     """Slicing"""
     name = "John Smith"
     print(name[2:4])
 
 
-def exercice_8():
+def exercise_8():
     """More on Indexing"""
     letters = 'abcdefghijklmnopqrstuvwxyz'
     print(letters[-2])
 
 
-def exercice_9():
+def exercise_9():
     """More on Slicing"""
     letters = 'abcdefghijklmnopqrstuvwxyz'
     print(letters[-3:-1])
 
 
-def exercice_10():
+def exercise_10():
     """List Indexing"""
     mylist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     print(mylist[17])
@@ -65,15 +65,15 @@ def exercice_10():
 
 def main():
     exercise_1()
-    exercice_2()
-    exercice_3()
-    exercice_4()
-    exercice_5()
-    exercice_6()
-    exercice_7()
-    exercice_8()
-    exercice_9()
-    exercice_10()
+    exercise_2()
+    exercise_3()
+    exercise_4()
+    exercise_5()
+    exercise_6()
+    exercise_7()
+    exercise_8()
+    exercise_9()
+    exercise_10()
 
 
 if __name__ == "__main__":
