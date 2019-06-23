ea0cb252cf83ce3c6410c1dc65f3cbd040dc27e4
zdenek-nemec <zdenek.nemec@artin.cz>
2019-06-23 10:59:15 +0200

    #10: Log levels

--- python-demo-logging/main_script.py
+++ python-demo-logging/main_script.py
@@ -8,10 +8,18 @@ import sys
 def main():
     """Demo of Python Logging module, see https://docs.python.org/3/howto/logging.html"""
 
-    logging.basicConfig(stream=sys.stdout)
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.warning("I am about to start")
 
     print("Hello, World!")
+
+    logging.debug("This is debug-level message")
+    logging.info("This is info-level message")
+    logging.warning("This is warning-level message")
+    logging.error("This is error-level message")
+    logging.critical("This is critical-level message")
+
+    print(logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL)
 
 
 if __name__ == "__main__":
--- python-demo-logging/presentation.md
+++ python-demo-logging/presentation.md
@@ -19,3 +19,18 @@ import sys
 
 logging.basicConfig(stream=sys.stdout)
 ```
+
+## 4. Log levels
+```
+logging.debug("This is debug-level message")
+logging.info("This is info-level message")
+logging.warning("This is warning-level message")
+logging.error("This is error-level message")
+logging.critical("This is critical-level message")
+```
+
+```
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+print(logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL)
+```
