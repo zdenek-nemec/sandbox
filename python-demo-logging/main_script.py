f38b7d31b6ca7b0979a6675fbb0ec05f31188b87
zdenek-nemec <zdenek.nemec@artin.cz>
2019-06-23 10:44:19 +0200

    #10: Template

---
+++ python-demo-logging/main_script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def main():
+    """Demo of Python Logging module, see https://docs.python.org/3/howto/logging.html"""
+
+if __name__ == "__main__":
+    main()
---
+++ python-demo-logging/presentation.md
@@ -0,0 +1,6 @@
+# Python Logging Demo
+
+## 1. Hello World
+```
+print("Hello, World!")
+```
