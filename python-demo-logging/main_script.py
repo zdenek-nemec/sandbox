556453b6e99aa68308bf1cc9801cd7a7fb7cdaf4
zdenek-nemec <zdenek.nemec@artin.cz>
2019-06-23 10:51:49 +0200

    #10: Start logging

--- python-demo-logging/main_script.py
+++ python-demo-logging/main_script.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
+
+import logging
+
+
 def main():
     """Demo of Python Logging module, see https://docs.python.org/3/howto/logging.html"""
+    logging.basicConfig(filename="my_log.log")
+    logging.warning("I am about to start")
+
+    print("Hello, World!")
+
 
 if __name__ == "__main__":
     main()
--- python-demo-logging/presentation.md
+++ python-demo-logging/presentation.md
@@ -4,3 +4,11 @@
 ```
 print("Hello, World!")
 ```
+
+## 2. Start logging
+```
+import logging
+
+logging.basicConfig(filename="my_log.log")
+logging.warning("I am about to start")
+```
