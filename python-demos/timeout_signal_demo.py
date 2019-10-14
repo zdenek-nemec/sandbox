559f8ad82cd00638536d5d549fe56be21ec73ce7
zdenek-nemec <zdenek.nemec@artin.cz>
2019-10-14 11:25:06 +0200

    #12: Python timeout demonstration with Signal module

---
+++ python-demos/timeout_signal_demo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+import signal
+import time
+
+
+def handler(signum, frame):
+    print("Forever is over!")
+    raise Exception("End of time")
+
+
+def loop_forever():
+    while True:
+        print("Waiting...")
+        time.sleep(1)
+
+
+def main():
+    """Python timeout demonstration with Signal module. Works on Unix only!"""
+    print("Hello, World!")
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(10)
+    try:
+        loop_forever()
+    except Exception as exc:
+        print(exc)
+    signal.alarm(0)
+
+
+if __name__ == "__main__":
+    main()
