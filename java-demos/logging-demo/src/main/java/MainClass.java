dd8f6789bc9225b5ba730da0c641e3a616f15c6c
zdenek-nemec <zdenek.nemec@artin.cz>
2020-05-25 17:54:28 +0200

    #20: Different level of log messages

--- java-demos/logging-demo/src/main/java/MainClass.java
+++ java-demos/logging-demo/src/main/java/MainClass.java
@@ -1,5 +1,16 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.INFO);
+        logger.severe("This is severe-level message.");
+        logger.warning("This is warning-level message.");
+        logger.info("This is info-level message.");
+        logger.config("This is config-level message.");
+        logger.fine("This is fine-level message.");
+        logger.finer("This is finer-level message.");
+        logger.finest("This is finest-level message.");
     }
 }
