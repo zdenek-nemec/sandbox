cc6a5cc0fe69fc3e4760aa0a3f925ad23a1d7c48
zdenek-nemec <zdenek.nemec@artin.cz>
2020-05-25 18:07:37 +0200

    #20: Using logger in other methods.

--- java-demos/logging-demo/README.md
+++ java-demos/logging-demo/README.md
@@ -1,1 +1,5 @@
 # Java Logging Demo
+
+Sources
+
+* [Vogella Java Logging API Tutorial](https://www.vogella.com/tutorials/Logging/article.html)
--- java-demos/logging-demo/src/main/java/MainClass.java
+++ java-demos/logging-demo/src/main/java/MainClass.java
@@ -1,16 +1,24 @@
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class MainClass {
+    private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         logger.setLevel(Level.INFO);
-        logger.severe("This is severe-level message.");
-        logger.warning("This is warning-level message.");
-        logger.info("This is info-level message.");
-        logger.config("This is config-level message.");
-        logger.fine("This is fine-level message.");
-        logger.finer("This is finer-level message.");
-        logger.finest("This is finest-level message.");
+        logger.severe("This is severe-level message in MainClass.main().");
+        logger.warning("This is warning-level message in MainClass.main().");
+        logger.info("This is info-level message in MainClass.main().");
+        logger.config("This is config-level message in MainClass.main().");
+        logger.fine("This is fine-level message in MainClass.main().");
+        logger.finer("This is finer-level message in MainClass.main().");
+        logger.finest("This is finest-level message in MainClass.main().");
+
+        otherMethod();
+    }
+
+    private static void otherMethod() {
+        logger.info("This is info-level message in MainClass.otherMethod().");
     }
 }
