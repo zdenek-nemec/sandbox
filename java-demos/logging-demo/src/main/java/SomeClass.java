ed41adf75fcbf0ea8fbd0aa08fc10c3d5e2e6582
zdenek-nemec <zdenek.nemec@artin.cz>
2020-05-30 15:34:11 +0200

    #19: Java logging code cleanup

--- java-demos/logging-demo/src/main/java/SomeClass.java
+++ java-demos/logging-demo/src/main/java/SomeClass.java
@@ -1,7 +1,7 @@
 import java.util.logging.Logger;
 
 public class SomeClass {
-    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private final static Logger LOGGER = Logger.getLogger("global");
 
     public static void someMethod() {
         LOGGER.info("Logger Name: " + LOGGER.getName());
