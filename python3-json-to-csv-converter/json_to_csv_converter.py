765212791a79c9d3a2413bfc43aab8a52a37edf7
zdenek-nemec <zdenek.nemec@artin.cz>
2020-07-18 14:35:38 +0200

    #24: JSON-CSV conversion prototype

--- python3-json-to-csv-converter/json_to_csv_converter.py
+++ python3-json-to-csv-converter/json_to_csv_converter.py
@@ -1,2 +1,38 @@
+import csv
+import json
+
+
+def load_json_data(path):
+    with open(path) as json_file:
+        return json.load(json_file)
+
+
+def print_records(records):
+    for record in records:
+        print(record)
+
+
+def get_header(records):
+    header = []
+    for record in records:
+        for key in record.keys():
+            if not key in header:
+                header.append(key)
+        break
+    return header
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    records = load_json_data("./test_data.json")
+    header = get_header(records)
+    with open("output.csv", "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",")
+        csv_writer.writerow(header)
+        for record in records:
+            row = []
+            for column in header:
+                if column in record.keys():
+                    row.append(record[column])
+                else:
+                    row.append("")
+            csv_writer.writerow(row)
