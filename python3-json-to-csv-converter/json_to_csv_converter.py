02a86fe933ca9bd313caadec9648b603ba5a2c81
zdenek-nemec <zdenek.nemec@artin.cz>
2020-07-18 16:51:42 +0200

    #24: Support for arguments, logging

--- python3-json-to-csv-converter/json_to_csv_converter.py
+++ python3-json-to-csv-converter/json_to_csv_converter.py
@@ -1,5 +1,8 @@
+import argparse
 import csv
 import json
+import logging
+import sys
 
 
 def load_json_data(path):
@@ -38,8 +41,33 @@ def create_csv_file(filename, records):
 
 
 def convert_json_to_csv():
-    records = load_json_data("./test_data.json")
+    argument_parser = argparse.ArgumentParser(prog="JSON to CSV Converter")
+    argument_parser.add_argument("--log_file")
+    argument_parser.add_argument(
+        "--log_level",
+        default="WARNING",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+    argument_parser.add_argument("--input_file", "-i")
+
+    log_file = argument_parser.parse_args().log_file
+    log_level = getattr(logging, argument_parser.parse_args().log_level, None)
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    if log_file == None:
+        logging.basicConfig(stream=sys.stdout, level=log_level, format=log_format)
+    else:
+        logging.basicConfig(filename=log_file, level=log_level, format=log_format)
+
+    input_file = argument_parser.parse_args().input_file
+
+    logging.debug("Application started")
+    logging.debug("Argument --log_file = %s" % log_file)
+    logging.debug("Argument --log_level = %s" % log_level)
+    logging.debug("Argument --input_file = %s" % input_file)
+
+    records = load_json_data(input_file)
     create_csv_file("output.csv", records)
+
+    logging.debug("Application finished")
 
 
 if __name__ == "__main__":
