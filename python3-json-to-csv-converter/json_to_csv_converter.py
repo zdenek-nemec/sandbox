75aa19f2484840281859e4731925e7086494ed0d
zdenek-nemec <zdenek.nemec@artin.cz>
2020-09-28 20:26:00 +0200

    #24: Refactoring

--- python3-json-to-csv-converter/json_to_csv_converter.py
+++ python3-json-to-csv-converter/json_to_csv_converter.py
@@ -6,14 +6,45 @@ import os
 import sys
 
 
-def load_json_data(path):
+def load_json_file(path):
     with open(path) as json_file:
         return json.load(json_file)
+
+
+def save_csv_file(path, data):
+    with open(path, "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+        for record in data:
+            csv_writer.writerow(record)
 
 
 def print_records(records):
     for record in records:
         print(record)
+
+
+def extract_json_records(csv_columns, json_data):
+    csv_data = []
+    row = []
+    for column in csv_columns:
+        row.append(column[0])
+    csv_data.append(row)
+
+    for record in json_data:
+        row = []
+        for column in csv_columns:
+            focus = record
+            for level in column[1]:
+                if level in focus.keys():
+                    focus = focus[level]
+                else:
+                    logging.debug("Value for %s not found" % column[0])
+                    row.append("")
+                    break
+            else:
+                row.append(focus)
+        csv_data.append(row)
+    return csv_data
 
 
 def main():
@@ -42,10 +73,9 @@ def main():
     input_file = "test_data.json"
 
     logging.debug("Loading input file %s" % input_file)
-    json_data = load_json_data(input_file)
+    json_data = load_json_file(input_file)
     logging.debug("Loaded %d records" % len(json_data))
     output_file = os.path.splitext(input_file)[0] + ".csv"
-
     # print_records(json_data)
 
     csv_columns = [
@@ -54,37 +84,14 @@ def main():
         ("CALLED_IMSI", ["called", "imsi"]),
     ]
 
-    csv_data = []
-    row = []
-    for column in csv_columns:
-        row.append(column[0])
-    csv_data.append(row)
-
-    for record in json_data:
-        row = []
-        for column in csv_columns:
-            focus = record
-            for level in column[1]:
-                if level in focus.keys():
-                    focus = focus[level]
-                else:
-                    logging.debug("Value for %s not found" % column[0])
-                    row.append("")
-                    break
-            else:
-                row.append(focus)
-        csv_data.append(row)
-
+    csv_data = extract_json_records(csv_columns, json_data)
     # print(csv_data)
 
-    with open("test_data.csv", "w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
-        for record in csv_data:
-            csv_writer.writerow(record)
+    save_csv_file(output_file, csv_data)
 
-        logging.debug("Created output file %s" % output_file)
+    logging.debug("Created output file %s" % output_file)
 
-        logging.debug("Application finished")
+    logging.debug("Application finished")
 
 
 if __name__ == "__main__":
