278b20dd27009ab4f7059e9b3c988612197011ae
zdenek-nemec <zdenek.nemec@artin.cz>
2020-10-04 21:39:09 +0200

    #24: JSON-CSV data conversion via JsonToCsv class

--- python3-json-to-csv-converter/json_to_csv_converter.py
+++ python3-json-to-csv-converter/json_to_csv_converter.py
@@ -24,29 +24,30 @@ class JsonToCsv(object):
         with open(path) as json_file:
             self._json_data = json.load(json_file)
 
+    def set_csv_columns(self, csv_columns):
+        self._csv_columns = csv_columns
 
-def extract_csv_data(csv_columns, json_data):
-    csv_data = []
-    row = []
-    for column in csv_columns:
-        row.append(column[0])
-    csv_data.append(row)
-
-    for record in json_data:
+    def extract_csv_data(self):
+        self._csv_data = []
         row = []
-        for column in csv_columns:
-            focus = record
-            for level in column[1]:
-                if level in focus.keys():
-                    focus = focus[level]
+        for column in self._csv_columns:
+            row.append(column[0])
+        self._csv_data.append(row)
+
+        for record in self._json_data:
+            row = []
+            for column in self._csv_columns:
+                focus = record
+                for level in column[1]:
+                    if level in focus.keys():
+                        focus = focus[level]
+                    else:
+                        logging.debug("Value for %s not found" % column[0])
+                        row.append("")
+                        break
                 else:
-                    logging.debug("Value for %s not found" % column[0])
-                    row.append("")
-                    break
-            else:
-                row.append(focus)
-        csv_data.append(row)
-    return csv_data
+                    row.append(focus)
+            self._csv_data.append(row)
 
 
 def save_csv_file(path, data):
@@ -92,20 +93,17 @@ def main():
 
     os.chdir(config.get_path())
     input_files = glob.glob(config.get_mask())
-    csv_columns = config.get_csv_columns()
+    json_to_csv = JsonToCsv()
+    json_to_csv.set_csv_columns(config.get_csv_columns())
 
     for input_file in input_files:
         logging.info("Loading input file %s" % input_file)
-        json_to_csv = JsonToCsv()
         json_to_csv.load_file(input_file)
-        json_data = json_to_csv._json_data
-        logging.info("JSON records: %d" % len(json_data))
-
-        csv_data = extract_csv_data(csv_columns, json_data)
-
+        logging.debug("JSON records: %d" % len(json_to_csv._json_data))
+        json_to_csv.extract_csv_data()
+        logging.debug("CSV records: %d" % len(json_to_csv._csv_data))
         output_file = os.path.splitext(input_file)[0] + ".csv"
-        save_csv_file(output_file, csv_data)
-
+        save_csv_file(output_file, json_to_csv._csv_data)
         logging.info("Created output file %s" % output_file)
 
     logging.info("Application finished")
