1afe5541545943e2a3ad7d2669e8f63549761992
zdenek-nemec <zdenek.nemec@artin.cz>
2020-10-04 20:59:40 +0200

    #24: Load JSON files based on mask

--- python3-json-to-csv-converter/json_to_csv_converter.py
+++ python3-json-to-csv-converter/json_to_csv_converter.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import glob
 import json
 import logging
 import os
@@ -22,18 +23,6 @@ class JsonToCsv(object):
     def load_file(self, path):
         with open(path) as json_file:
             self._json_data = json.load(json_file)
-
-
-def save_csv_file(path, data):
-    with open(path, "w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
-        for record in data:
-            csv_writer.writerow(record)
-
-
-def print_records(records):
-    for record in records:
-        print(record)
 
 
 def extract_csv_data(csv_columns, json_data):
@@ -60,15 +49,28 @@ def extract_csv_data(csv_columns, json_data):
     return csv_data
 
 
+def save_csv_file(path, data):
+    with open(path, "w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+        for record in data:
+            csv_writer.writerow(record)
+
+
+def print_records(records):
+    for record in records:
+        print(record)
+
+
 def main():
-    argument_parser = argparse.ArgumentParser(prog="JSON to CSV Converter")
+    argument_parser = argparse.ArgumentParser(prog="json_to_csv_converter")
     argument_parser.add_argument("--log_file")
     argument_parser.add_argument(
         "--log_level",
         default="DEBUG",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
     )
-    argument_parser.add_argument("--config_file", "-c")
+    # argument_parser.add_argument("--config", "-c", required=True)
+    argument_parser.add_argument("--config", "-c", required=False)  # Debug
 
     log_file = argument_parser.parse_args().log_file
     log_level = getattr(logging, argument_parser.parse_args().log_level, None)
@@ -77,39 +79,36 @@ def main():
         logging.basicConfig(stream=sys.stdout, level=log_level, format=log_format)
     else:
         logging.basicConfig(filename=log_file, level=log_level, format=log_format)
-    config_file = argument_parser.parse_args().config_file
+    config_file = argument_parser.parse_args().config
 
-    logging.debug("Application started")
+    logging.info("Application started")
     logging.debug("Argument --log_file = %s" % log_file)
     logging.debug("Argument --log_level = %s" % log_level)
-    logging.debug("Argument --config_file = %s" % config_file)
+    logging.debug("Argument --config = %s" % config_file)
 
     config = Config()
-    config.load("json_to_csv.cfg")
-    # config.generate("json_to_csv.cfg")
-    # config.save()
+    # config.read(config_file)
+    config.read("json_to_csv.cfg")  # Debug
 
-    config.print()
-    return
-
-    input_files = ["test_data.json"]
+    os.chdir(config.get_path())
+    input_files = glob.glob(config.get_mask())
     csv_columns = config.get_csv_columns()
 
     for input_file in input_files:
-        logging.debug("Loading input file %s" % input_file)
+        logging.info("Loading input file %s" % input_file)
         json_to_csv = JsonToCsv()
         json_to_csv.load_file(input_file)
         json_data = json_to_csv._json_data
-        logging.debug("JSON records: %d" % len(json_data))
+        logging.info("JSON records: %d" % len(json_data))
 
         csv_data = extract_csv_data(csv_columns, json_data)
 
         output_file = os.path.splitext(input_file)[0] + ".csv"
         save_csv_file(output_file, csv_data)
 
-        logging.debug("Created output file %s" % output_file)
+        logging.info("Created output file %s" % output_file)
 
-    logging.debug("Application finished")
+    logging.info("Application finished")
 
 
 if __name__ == "__main__":
