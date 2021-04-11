add3a40cd69541874c410bc514197e77412722a3
zdenek-nemec <zdenek.nemec@artin.cz>
2021-04-11 22:26:01 +0200

    Multiple sections in config file

--- python3-demos/config_parser_demo/config_parser_demo.py
+++ python3-demos/config_parser_demo/config_parser_demo.py
@@ -4,9 +4,12 @@ import configparser
 def main():
     config_parser = configparser.ConfigParser()
     config_parser.optionxform = str
-    config_parser.add_section("settings")
-    config_parser.set("settings", "a", "1")
-    config_parser.set("settings", "b", "2")
+    config_parser.add_section("section_a")
+    config_parser.set("section_a", "a1", "1")
+    config_parser.set("section_a", "a2", "2")
+    config_parser.add_section("section_b")
+    config_parser.set("section_b", "b1", "1")
+    config_parser.set("section_b", "b2", "2")
     try:
         with open("config_output.cfg", "w") as config_file:
             config_parser.write(config_file)
