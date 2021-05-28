1b4c9f6e0e521412feba05bd948906ec5833ff69
zdenek-nemec <zdenek.nemec@artin.cz>
2021-05-28 15:56:59 +0200

    ASA: Random data generator ready

--- adaptive-statistics-alarm/sandbox.py
+++ adaptive-statistics-alarm/sandbox.py
@@ -1,17 +1,22 @@
-def generate_random_data(filename, entries):
-    data = []
-    for i in range(0, entries):
-        data.append((i, 0))
-    return data
+import datetime
+import random
+
+
+DAYS_TO_GENERATE = 10
+
+
+def generate_random_data(entries):
+    timestamps = [(datetime.datetime.strptime("2021-05-01 0:00:00", "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=x)) for x in range(entries)]
+    values = random.sample(range(0,1000), entries)
+    return [(timestamps[i], values[i]) for i in range(entries)]
 
 
 def main():
     print("Hello, Adaptive Statistics Alarm!")
 
-    data = generate_random_data("random_data_sample.csv", 10)
-    print(data)
-
-    # Generate random data with timestamps
+    data = generate_random_data(DAYS_TO_GENERATE * 24)
+    # for entry in data:
+    #     print(entry[0].strftime("%Y-%m-%d %H:%M:%S"), ",", entry[1], sep="")
 
     # Save random data to CSV
 
