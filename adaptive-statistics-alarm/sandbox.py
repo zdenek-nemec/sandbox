0b5b9afa600d5dae29a03f2d2bb4f89d897f37a2
zdenek-nemec <zdenek.nemec@artin.cz>
2021-05-28 15:17:24 +0200

    ASA: Preparation of random data generator

--- adaptive-statistics-alarm/sandbox.py
+++ adaptive-statistics-alarm/sandbox.py
@@ -1,1 +1,28 @@
-print("Hello, Adaptive Statistics Alarm!")
+def generate_random_data(filename, entries):
+    data = []
+    for i in range(0, entries):
+        data.append((i, 0))
+    return data
+
+
+def main():
+    print("Hello, Adaptive Statistics Alarm!")
+
+    data = generate_random_data("random_data_sample.csv", 10)
+    print(data)
+
+    # Generate random data with timestamps
+
+    # Save random data to CSV
+
+    # Load data from CSV
+
+    # Best match data timestamps
+
+    # Calculate variance
+
+    # Alarm if variance raises too much
+
+
+if __name__ == "__main__":
+    main()
