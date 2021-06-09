a7c2c5f6ed5aa6a5a036d4f054dcebdad6b5b932
zdenek-nemec <zdenek.nemec@artin.cz>
2021-06-09 21:01:55 +0200

    Bash: Colours demo

---
+++ bash-demos/colours_demo/colours.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Name: colours.sh
+#
+# Synopsis:
+#     colours.sh
+#
+# Examples:
+#     colours.sh
+#
+# Description:
+#     Shell script for demonstration of coloured text in Bash.
+#
+
+printf "Colours of Bash:\n"
+printf "* \033[0;30m Black        \033[0m Black\n"
+printf "* \033[0;31m Red          \033[0m Red\n"
+printf "* \033[0;32m Green        \033[0m Green\n"
+printf "* \033[0;33m Brown/Orange \033[0m Brown/Orange\n"
+printf "* \033[0;34m Blue         \033[0m Blue\n"
+printf "* \033[0;35m Purple       \033[0m Purple\n"
+printf "* \033[0;36m Cyan         \033[0m Cyan\n"
+printf "* \033[0;37m Light Gray   \033[0m Light Gray\n"
+printf "* \033[1;30m Dark Gray    \033[0m Dark Gray\n"
+printf "* \033[1;31m Light Red    \033[0m Light Red\n"
+printf "* \033[1;32m Light Green  \033[0m Light Green\n"
+printf "* \033[1;33m Yellow       \033[0m Yellow\n"
+printf "* \033[1;34m Light Blue   \033[0m Light Blue\n"
+printf "* \033[1;35m Light Purple \033[0m Light Purple\n"
+printf "* \033[1;36m Light Cyan   \033[0m Light Cyan\n"
+printf "* \033[1;37m White        \033[0m White\n"
