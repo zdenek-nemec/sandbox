d1b8d6658f190e791e8ae0396e94483a4066a065
zdenek-nemec <zdenek.nemec@artin.cz>
2021-06-10 23:52:31 +0200

    Outlier Detector: Min-Max

--- outlier-detector/analyse_data.py
+++ outlier-detector/analyse_data.py
@@ -2,13 +2,36 @@ import datetime
 import csv
 
 
-DATA_FILE = "random_data.csv"
+# DATA_FILENAME_1 = "random_data.csv"
+DATA_FILENAME_1 = "sip_2021-04.csv"
+DATA_FILENAME_2 = "sip_2021-05.csv"
 
 
 class HardLimit(object):
     def __init__(self, minimum, maximum):
         self._minimum = minimum
         self._maximum = maximum
+
+    def get_outliers(self, data):
+        outliers = []
+        for entry in data:
+            if entry[1] < self._minimum or entry[1] > self._maximum:
+                outliers.append(entry)
+        return outliers
+
+
+class MinMax(object):
+    def __init__(self):
+        self._minimum = None
+        self._maximum = None
+
+    def learn(self, data):
+        self._minimum = self._maximum = data[0][1]
+        for entry in data:
+            if entry[1] < self._minimum:
+                self._minimum = entry[1]
+            if entry[1] > self._maximum:
+                self._maximum = entry[1]
 
     def get_outliers(self, data):
         outliers = []
@@ -32,18 +55,47 @@ def get_time_of_day_data(data):
 def main():
     print("Hello, Analyse Data!")
 
-    data = []
-    with open(DATA_FILE, "r") as csv_file:
+    data1 = []
+    with open(DATA_FILENAME_1, "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         for row in reader:
-            data.append((datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S"), int(row[1])))
-    # [print(x) for x in sorted(data)]
+            data1.append((datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S"), int(row[1])))
+    # [print(x) for x in sorted(data1)]
 
-    outliers = HardLimit(100, 999).get_outliers(data)
-    print("HardLimit:", len(outliers))
-    output_filename = DATA_FILE[:-4] + "_hard_limit.txt"
+    data2 = []
+    with open(DATA_FILENAME_2, "r") as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        for row in reader:
+            data2.append((datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S"), int(row[1])))
+    # [print(x) for x in sorted(data2)]
+
+    outliers = HardLimit(100, 999).get_outliers(data1)
+    print("HardLimit 1:", len(outliers))
+    output_filename = DATA_FILENAME_1[:-4] + "_hard_limit.txt"
     with open(output_filename, "w") as text_file:
-        for entry in data:
+        for entry in data1:
+            if entry in outliers:
+                text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+            else:
+                text_file.write("      " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+
+    outliers = HardLimit(100, 999).get_outliers(data2)
+    print("HardLimit 2:", len(outliers))
+    output_filename = DATA_FILENAME_2[:-4] + "_hard_limit.txt"
+    with open(output_filename, "w") as text_file:
+        for entry in data2:
+            if entry in outliers:
+                text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+            else:
+                text_file.write("      " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+
+    min_max = MinMax()
+    min_max.learn(data1)
+    outliers = min_max.get_outliers(data2)
+    print("MinMax 2:", len(outliers))
+    output_filename = DATA_FILENAME_2[:-4] + "_min_max.txt"
+    with open(output_filename, "w") as text_file:
+        for entry in data2:
             if entry in outliers:
                 text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
             else:
