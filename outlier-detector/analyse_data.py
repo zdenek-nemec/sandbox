7cfb92033612aa645c6762a9e1723e81a66b0356
zdenek-nemec <zdenek.nemec@artin.cz>
2021-06-11 08:13:52 +0200

    Outlier Detector: Learning Deviance

--- outlier-detector/analyse_data.py
+++ outlier-detector/analyse_data.py
@@ -1,10 +1,34 @@
-import datetime
 import csv
+import datetime
+import statistics
 
 
 # DATA_FILENAME_1 = "random_data.csv"
-DATA_FILENAME_1 = "sip_2021-04.csv"
-DATA_FILENAME_2 = "sip_2021-05.csv"
+# DATA_FILENAME_1 = "sip_2021-04.csv"
+# DATA_FILENAME_2 = "sip_2021-05.csv"
+DATA_FILENAME_1 = "sip_2021-04_train.csv"
+DATA_FILENAME_2 = "sip_2021-04_data.csv"
+
+WEEKEND_DAYS = [
+    "2021-04-03",
+    "2021-04-04",
+    "2021-04-10",
+    "2021-04-11",
+    "2021-04-17",
+    "2021-04-18",
+    "2021-04-24",
+    "2021-04-25",
+    "2021-05-01",
+    "2021-05-02",
+    "2021-05-08",
+    "2021-05-09",
+    "2021-05-15",
+    "2021-05-16",
+    "2021-05-22",
+    "2021-05-23",
+    "2021-05-29",
+    "2021-05-30",
+]
 
 
 class HardLimit(object):
@@ -64,6 +88,87 @@ class MinMaxHour(object):
                 if entry[1] < self._hours[hour][0] or entry[1] > self._hours[hour][1]:
                     outliers.append(entry)
         return outliers
+
+
+class Deviance(object):
+    def __init__(self):
+        self._hours = {}
+
+    def learn(self, data):
+        for entry in data:
+            hour = entry[0].strftime("%H")
+            if hour not in self._hours:
+                self._hours[hour] = [entry[1]]
+            else:
+                self._hours[hour].append(entry[1])
+
+    def get_outliers(self, data):
+        outliers = []
+        for entry in data:
+            hour = entry[0].strftime("%H")
+            if hour not in self._hours:
+                continue
+            else:
+                hour_list = self._hours[hour]
+                if statistics.pstdev(hour_list + [entry[1]]) > statistics.pstdev(hour_list):
+                    outliers.append(entry)
+        return outliers
+
+
+class LearningDeviance(object):
+    def __init__(self):
+        self._hours = {}
+
+    def learn(self, data):
+        for entry in data:
+            if is_weekend(entry):
+                continue
+            hour = entry[0].strftime("%H")
+            if hour not in self._hours:
+                self._hours[hour] = [entry[1]]
+            else:
+                self._hours[hour].append(entry[1])
+
+    def get_outliers(self, data):
+        outliers = []
+        for entry in data:
+            if is_weekend(entry):
+                continue
+            hour = entry[0].strftime("%H")
+            if hour not in self._hours:
+                self._hours[hour] = [entry[1]]
+            else:
+                hour_list = self._hours[hour]
+                current_deviance = statistics.pstdev(hour_list)
+                new_deviance = statistics.pstdev(hour_list + [entry[1]])
+                if new_deviance > current_deviance and abs(new_deviance - current_deviance) > 1000:
+                    outliers.append(entry)
+                    print(hour)
+                    print(sorted(hour_list))
+                    print(entry[1])
+                    print(abs(new_deviance - current_deviance))
+                self._hours[hour].append(entry[1])
+                # else:
+                #     print(abs(new_deviance - current_deviance))
+        return outliers
+
+
+def save_report(report_name, data, outliers):
+    output_filename = DATA_FILENAME_2[:-4] + "_" + report_name + ".txt"
+    with open(output_filename, "w") as text_file:
+        for entry in data:
+            if entry in outliers:
+                text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+            else:
+                text_file.write("      " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+
+
+def is_weekend(data_entry):
+    day = hour = data_entry[0].strftime("%Y-%m-%d")
+    if day in WEEKEND_DAYS:
+        return True
+    else:
+        return False
 
 
 def main():
@@ -126,6 +231,24 @@ def main():
                 text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
             else:
                 text_file.write("      " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+
+    deviance = Deviance()
+    deviance.learn(data1)
+    outliers = deviance.get_outliers(data2)
+    print("Deviance 2:", len(outliers))
+    output_filename = DATA_FILENAME_2[:-4] + "_deviance.txt"
+    with open(output_filename, "w") as text_file:
+        for entry in data2:
+            if entry in outliers:
+                text_file.write("Alarm " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+            else:
+                text_file.write("      " + str(entry[0]) + ", " + str(entry[1]) + "\n")
+
+    learning_deviance = LearningDeviance()
+    learning_deviance.learn(data1)
+    outliers = learning_deviance.get_outliers(data2)
+    print("LearningDeviance 2:", len(outliers))
+    save_report("learning_deviance", data2, outliers)
 
 
 if __name__ == "__main__":
