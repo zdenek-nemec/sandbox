c05c8963341153fe1efa08a4208864a5467cd6fc
zdenek-nemec <zdenek.nemec@artin.cz>
2021-06-19 22:28:46 +0200

    #31: Updated template

--- bash-demos/template/hello.sh
+++ bash-demos/template/hello.sh
@@ -1,78 +1,67 @@
 #!/bin/bash
 #
-# Name: script.sh
+# Name: hello.sh
 #
 # Synopsis:
-#     script.sh [-h] [-n NAME]
+#     hello.sh [-h] [-n NAME] [-g {Hello,Hi}]
 #
 # Examples:
-#     script.sh
-#     script.sh -n Arcifrajer
+#     hello.sh
+#     hello.sh -n Arcifrajer
 #
 # Description:
-#     Shell script for the Arcifrajer.
+#     Hello, Arcifrajer!
 #
 #     -h, --help
 #         Show this help message and exit
 #     -n NAME, --name NAME
 #         User's name
+#     -g {Hello,Hi}, --greeting {Hello,Hi}
+#         Specific greeting
 #
 
 DEFAULT_NAME="Frajer"
+DEFAULT_GREETING="Hello"
 
 function print_usage {
-    echo "Usage: `basename $0` [-h] [-n NAME]"
-    echo "Try \``basename $0` --help' for more information."
+    echo "Usage: $(basename $BASH_SOURCE) [-h] [-n NAME] [-g {Hello,Hi}]"
+    echo "Try \`$(basename $BASH_SOURCE) --help' for more information."
 }
 
 function print_help {
-    head -`grep -n -m 1 -v "^#" "$0" | cut -d ":" -f 1` "$0"
+    head -$(grep -n -m 1 -v "^#" "$BASH_SOURCE" | cut -d ":" -f 1) "$BASH_SOURCE"
 }
 
-# # Help
-# if [[ "$#" == "1" && ("$1" == "-h" || "$1" == "--help") ]]; then
-#     print_help
-#     exit 0
-# fi
-
-# # Arguments
-# if [[ "$#" == "0" ]]; then
-#     name=$DEFAULT_NAME
-# elif [[ "$#" == "2" && ("$1" == "-n" || "$1" == "--name") ]]; then
-#     name=$2
-# else
-#     echo "Error: Unexpected arguments" 1>&2
-#     print_usage
-#     exit -1
-# fi
-
-
 name=$DEFAULT_NAME
+greeting=$DEFAULT_GREETING
 while [[ $# -gt 0 ]]; do
     option="$1"
     case $option in
         -h|--help)
-            shift
             print_help
+            shift
             exit 0
             ;;
         -n|--name)
-            [ -z $2 ] && echo "error"; exit -1
+            [[ -z $2 ]] && echo "Error: Missing name value" 1>&2 && print_usage && exit -1
             name="$2"
             shift
             shift
             ;;
-        *)
+        -g|--greeting)
+            [[ -z $2 ]] && echo "Error: Missing greeintg value" 1>&2 && print_usage && exit -1
+            [[ $2 != "Hello" && $2 != "Hi" ]] && echo "Error: Invalid greeting value $2" 1>&2 && print_usage && exit -1
+            greeting="$2"
             shift
+            shift
+            ;;
+        *)
             echo "Error: Unexpected arguments" 1>&2
             print_usage
+            shift
             exit -1
             ;;
     esac
 done
 
-
-
-# Execute
-
-echo "Hello, ${name}!"
+echo "${greeting}, ${name}!"
