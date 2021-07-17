60db5325c7ca63358dbe5b778f853d927223dad8
zdenek-nemec <zdenek.nemec@artin.cz>
2021-07-17 22:09:04 +0200

    #49: Store statistical data in dictionary

--- outlier-detector/sip_statistics_check.py
+++ outlier-detector/sip_statistics_check.py
@@ -8,7 +8,7 @@ INPUT_DATA_FILENAME_1 = "sip_2021-06-01_00.csv"
 def main():
     print("Hello, SIP Statistics Check!")
 
-    data = []
+    data = {}
     with open(INPUT_DATA_FILENAME_1, "r") as csv_file:
         reader = csv.reader(csv_file, delimiter=",")
         for row in reader:
@@ -17,10 +17,14 @@ def main():
             elif row[0][0] == "#":
                 continue
             else:
-                timestamp = "-".join([row[4], row[2], row[3], row[1]])
+                timestamp_string = "-".join([row[4], row[2], row[3], row[1]])
+                timestamp = datetime.datetime.strptime(timestamp_string, "%Y-%b-%d-%H")
                 records = int(row[0])
-                data.append((datetime.datetime.strptime(timestamp, "%Y-%b-%d-%H"), records))
-    [print(entry) for entry in data]
+                if timestamp not in data:
+                    data[timestamp] = records
+                else:
+                    data[timestamp] = data[timestamp] + records
+    [print(key, data[key]) for key in data]
 
 
 if __name__ == "__main__":
