67f7641314f014eec4d9c12ee54bb2057a86833d
zdenek-nemec <zdenek.nemec@artin.cz>
2021-10-28 20:50:38 +0200

    #69: Example with arguments

--- python-demos/binec/timeout_multiprocessing_demo.py
+++ python-demos/binec/timeout_multiprocessing_demo.py
@@ -2,23 +2,26 @@ import multiprocessing
 import time
 
 
-def loop_forever():
+def loop_forever(a, b, c):
     while True:
-        print("Waiting...")
+        print("Waiting... %d %s %d" % (a, b, c))
+        a += 1
+        # if a >= 4:
+        #     return
         time.sleep(1)
 
 
 def main():
     """Python timeout demonstration with Multiprocessing module."""
     print("Hello, World!")
-    process = multiprocessing.Process(target=loop_forever)
+    process = multiprocessing.Process(target=loop_forever, args=(1, "2222", 3))
     process.start()
     process.join(10)
     if process.is_alive():
         print("It is still alive, let's kill it!")
         process.terminate()
         process.join()
-
+    print("Finished")
 
 if __name__ == "__main__":
     main()
