66f8f99e2a6057a8cd84abe693cc174349ee6f84
zdenek-nemec <zdenek.nemec@artin.cz>
2022-02-10 17:08:39 +0100

    #79: Selenium code cleanup

--- JavaSelenium/src/main/java/JavaSelenium.java
+++ JavaSelenium/src/main/java/JavaSelenium.java
@@ -3,10 +3,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaSelenium {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://selenium.dev");
-        String title = driver.getTitle();
-        System.out.println("The title is " + title);
-        driver.quit();
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://selenium.dev");
+        String title = webDriver.getTitle();
+        System.out.println("The title of the page is " + title);
+        webDriver.quit();
     }
 }
