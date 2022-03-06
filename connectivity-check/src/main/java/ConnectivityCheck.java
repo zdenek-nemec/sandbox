8c0981bc793554eeb88934a10e2c352e62829a45
zdenek-nemec <zdenek.nemec@artin.cz>
2022-03-06 22:08:48 +0100

    #81: List of SFTP targets

--- connectivity-check/src/main/java/ConnectivityCheck.java
+++ connectivity-check/src/main/java/ConnectivityCheck.java
@@ -1,11 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ConnectivityCheck {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello, Connectivity Check!");
 
-        String[] target_list = {"avl4658t", "localhost"};
+        List<String> target_list = new ArrayList<>();
+        try {
+            File file = new File("sftp_targets.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                target_list.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot read input file");
+            e.printStackTrace();
+        }
 
         System.out.println("Target,Connectivity Test");
         for (String target : target_list) {
