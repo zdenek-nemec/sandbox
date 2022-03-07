29a645813fd4883baa1872ba98da1649c29b5eb4
zdenek-nemec <zdenek.nemec@artin.cz>
2022-03-07 15:58:38 +0100

    #81: Michael's review

--- connectivity-check/src/main/java/cz/zdenek/sandbox/connectivitycheck/ConnectivityCheck.java
+++ connectivity-check/src/main/java/cz/zdenek/sandbox/connectivitycheck/ConnectivityCheck.java
@@ -1,34 +1,33 @@
-import java.io.File;
+package cz.zdenek.sandbox.connectivitycheck;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ConnectivityCheck {
     public static void main(String[] args) throws IOException {
+        new ConnectivityCheck().runCheck();
+    }
+
+    private void runCheck() throws IOException {
         System.out.println("Hello, Connectivity Check!");
 
-        List<String> target_list = new ArrayList<>();
+        List<String> target_list;
         try {
-            File file = new File("sftp_targets.txt");
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                target_list.add(line);
-            }
-            scanner.close();
+            target_list = Files.lines(Paths.get("sftp_targets.txt")).collect(Collectors.toList());
         } catch (FileNotFoundException e) {
             System.out.println("Cannot read input file");
-            e.printStackTrace();
+            throw e;
         }
 
         System.out.println("Target,Connectivity Test");
         for (String target : target_list) {
             System.out.print(target + ":22,");
-            try {
-                Socket clientSocket = new Socket(target, 22);
+            try (Socket clientSocket = new Socket(target, 22)) {
                 System.out.println("success");
             } catch (Exception e) {
                 System.out.println("failed");
