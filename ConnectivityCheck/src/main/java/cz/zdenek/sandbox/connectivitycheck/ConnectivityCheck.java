6dd8e7b16c27df013bacd3573ff9657f347ad5cb
zdenek-nemec <zdenek.nemec@artin.cz>
2022-03-26 23:34:05 +0100

    #81: Refactoring

--- ConnectivityCheck/src/main/java/cz/zdenek/sandbox/connectivitycheck/ConnectivityCheck.java
+++ ConnectivityCheck/src/main/java/cz/zdenek/sandbox/connectivitycheck/ConnectivityCheck.java
@@ -1,6 +1,5 @@
 package cz.zdenek.sandbox.connectivitycheck;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -10,33 +9,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConnectivityCheck {
+    private static final boolean DEBUG = false; // TODO: Is this an idiotic idea? Using build configuration is probably better idea.
+
     public static void main(String[] args) throws IOException {
-        new ConnectivityCheck().runConnectivityCheck(args);
+        if (DEBUG) {
+            new ConnectivityCheck().runConnectivityCheck(new String[]{"targets.csv"});
+        } else {
+            new ConnectivityCheck().runConnectivityCheck(args);
+        }
     }
 
     private void runConnectivityCheck(String[] args) throws IOException {
-        String target_list_file;
-        if (args.length == 1) {
-            target_list_file = args[0];
-        } else {
-            target_list_file = "targets.csv";
-        }
-
-        List<String> lines;
-        try {
-            lines = Files.lines(Paths.get(target_list_file)).collect(Collectors.toList());
-        } catch (FileNotFoundException e) {
-            System.out.println("Cannot read input file");
-            throw e;
-        }
-
-        List<Target> targets = new ArrayList<>();
-        for (String line : lines) {
-            if (line.startsWith("#")) {
-                continue;
-            }
-            targets.add(new Target(line));
-        }
+        List<Target> targets = getTargets(args);
 
         System.out.println("#Target,Connectivity Test,Login Test,Description");
         for (Target target : targets) {
@@ -52,5 +36,35 @@ public class ConnectivityCheck {
             }
             System.out.println(target.description);
         }
+    }
+
+    private List<Target> getTargets(String[] args) throws IOException {
+        String targetsFile = getTargetsFile(args);
+        List<String> lines = getLines(targetsFile);
+        return getTargetEntries(lines);
+    }
+
+    private String getTargetsFile(String[] args) {
+        return args[0];
+    }
+
+    private List<String> getLines(String filename) throws IOException {
+        try {
+            return Files.lines(Paths.get(filename)).collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("Cannot read input file");
+            throw e;
+        }
+    }
+
+    private List<Target> getTargetEntries(List<String> lines) {
+        List<Target> targets = new ArrayList<>();
+        for (String line : lines) {
+            if (line.startsWith("#")) {
+                continue;
+            }
+            targets.add(new Target(line));
+        }
+        return targets;
     }
 }
