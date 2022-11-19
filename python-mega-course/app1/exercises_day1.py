9f09609d5da22b1403f52f48e77a613f79224271
zdenek-nemec <zdenek.nemec@artin.cz>
2022-11-19 10:51:02 +0100

    #85: Day 1, basics

---
+++ python-mega-course/app1/exercises_day1.py
@@ -0,0 +1,24 @@
+def exercise1():
+    my_variable = "Ahoj"
+    print(type(my_variable))
+
+
+def exercise2():
+    user_input = input("Enter a number: ")
+    print(user_input)
+
+
+def exercise3():
+    items = [1, 2, 3, 4]
+    print(items)
+
+
+def main():
+    print("Hello, World!")
+    exercise1()
+    exercise2()
+    exercise3()
+
+
+if __name__ == "__main__":
+    main()
