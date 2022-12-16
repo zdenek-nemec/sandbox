1f84ee658ffe8e315b3ff4d3d121e4b80f71f312
zdenek-nemec <zdenek.nemec@artin.cz>
2022-12-16 17:29:27 +0100

    #93: Day 2, part one

---
+++ AdventOfCode2022/src/main/java/cz/zdenek/sandbox/adventofcode2022/day2/MatchResult.java
@@ -0,0 +1,9 @@
+package cz.zdenek.sandbox.adventofcode2022.day2;
+
+public class MatchResult {
+    enum Result {
+        LEFT_WINS,
+        RIGHT_WINS,
+        DRAW
+    }
+}
---
+++ AdventOfCode2022/src/test/java/cz/zdenek/sandbox/adventofcode2022/day2/RockPaperScissorsGuideTest.java
@@ -0,0 +1,30 @@
+package cz.zdenek.sandbox.adventofcode2022.day2;
+
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RockPaperScissorsGuideTest {
+    private static final List<String> EXAMPLE = Arrays.asList("A Y", "B X", "C Z");
+    private static RockPaperScissorsGuide RPS_GUIDE;
+
+    @BeforeClass
+    public static void setUp() {
+        RPS_GUIDE = new RockPaperScissorsGuide(EXAMPLE);
+    }
+
+    @AfterClass
+    public static void finished() {
+    }
+
+    @Test
+    public void testGetLength() {
+        Assert.assertEquals(3, RPS_GUIDE.getLength());
+    }
+
+    @Test
+    public void testGetAllRightPoints() {
+        Assert.assertEquals(Integer.valueOf(15), RPS_GUIDE.getAllRightPoints());
+    }
+}
