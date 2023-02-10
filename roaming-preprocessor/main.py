9baa00b83a6d06911650953a03ae1f8ade3b187a
zdenek-nemec <zdenek.nemec@artin.cz>
2023-02-10 07:56:36 +0100

    #95: Logging

--- roaming-preprocessor/main.py
+++ roaming-preprocessor/main.py
@@ -1,11 +1,32 @@
+import argparse
 import csv
+import logging
+import sys
+import timeit
 
+DEFAULT_LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 DEFAULT_INPUT_FILENAME = "c:/Zdenek/_tmp/roaming-preprocessor/2023012600_01782806.dat"
 DEFAULT_OUTPUT_FILENAME = "c:/Zdenek/_tmp/roaming-preprocessor/2023012600_01782806.csv"
 
 
 def main():
     print("Roaming Preprocessor")
+
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument(
+        "--log_level", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    )
+
+    log_level = argument_parser.parse_args().log_level
+    log_format = DEFAULT_LOG_FORMAT
+    logging.basicConfig(stream=sys.stdout, level=log_level, format=log_format)
+
+    logging.info("Application started")
+    application_start_time = timeit.default_timer()
+
+    logging.debug("Arguments: log_level = {0}".format(
+        argument_parser.parse_args().log_level
+    ))
 
     # Load configuration
 
@@ -35,6 +56,10 @@ def main():
             writer.writerow(row)
 
     # Save work file
+
+    application_stop_time = timeit.default_timer()
+    logging.debug("Finished in %.1fs" % (application_stop_time - application_start_time))
+    logging.info("Application finished")
 
 
 if __name__ == "__main__":
