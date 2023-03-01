eefe4a5705a0af5217c8f2d1a3e8bcb20e73c089
zdenek-nemec <zdenek.nemec@artin.cz>
2023-03-01 22:48:51 +0100

    #57: Application controller split to extra class

--- python-demos/template/main.py
+++ python-demos/template/main.py
@@ -1,31 +1,11 @@
-import argparse
 import logging
-import sys
 import timeit
 
+from application_controller import ApplicationController
 from application_lock import ApplicationLock
 from lucky_numbers import LuckyNumbers
 
 DEFAULT_NUMBER_LIST = [-3, 0, 3, 3.14159, 4 + 2j, 7, "Seven", 13, 21, 42]
-
-
-class ApplicationController(object):
-    """Template application controller"""
-
-    def __init__(self):
-        self._set_arguments()
-        self._set_logging()
-
-    def _set_arguments(self):
-        self._argument_parser = argparse.ArgumentParser()
-        self._argument_parser.add_argument(
-            "--log_level", default="WARNING", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-        )
-
-    def _set_logging(self):
-        log_level = self._argument_parser.parse_args().log_level
-        log_format = "%(asctime)s - %(levelname)s - %(message)s"
-        logging.basicConfig(stream=sys.stdout, level=log_level, format=log_format)
 
 
 def main():
