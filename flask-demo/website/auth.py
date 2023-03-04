8d1d664e993fa3cf00a568506a0e5bac0142f282
zdenek-nemec <zdenek.nemec@artin.cz>
2023-03-04 12:14:15 +0100

    #91: Login manager

--- flask-demo/website/auth.py
+++ flask-demo/website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -15,17 +16,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in successfully!", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for("views.home"))
             else:
                 flash("Incorrect password, try again.", category="error")
         else:
             flash("Email does not exist.", category="error")
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for("auth.login"))
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
@@ -45,11 +49,12 @@ def sign_up():
         elif password1 != password2:
             flash("Passwords do not match.", category="error")
         elif len(password1) < 7:
-            flash("Passworm must be at least 7 characters.", category="error")
+            flash("Password must be at least 7 characters.", category="error")
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for("views.home"))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
