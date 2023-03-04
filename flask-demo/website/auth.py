eac42043179c7f12a7fbad26e5f95a652f1f7810
zdenek-nemec <zdenek.nemec@artin.cz>
2023-03-04 11:48:44 +0100

    #91: Login

--- flask-demo/website/auth.py
+++ flask-demo/website/auth.py
@@ -8,8 +8,18 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    data = request.form
-    print(data)
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password, try again.", category="error")
+        else:
+            flash("Email does not exist.", category="error")
     return render_template("login.html")
 
 
@@ -25,7 +35,10 @@ def sign_up():
         first_name = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("This user exists already.", category="error")
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters.", category="error")
         elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category="error")
