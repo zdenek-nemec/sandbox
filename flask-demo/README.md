7e934444cbb42f4b5c291027a496d835754fdaa2
zdenek-nemec <zdenek.nemec@artin.cz>
2023-03-04 18:33:10 +0100

    #91: Deleting notes

--- flask-demo/README.md
+++ flask-demo/README.md
@@ -3,10 +3,28 @@
 * [YouTube](https://www.youtube.com/watch?v=dam0GPOAvVI&ab_channel=TechWithTim)
 * [GitHub](https://github.com/techwithtim/Flask-Web-App-Tutorial)
 
+## Install
+
 Set up the virtual environment
 
-```text
+```bash
+pip install -r requirements.txt
+```
+
+Or
+
+```bash
 pip install flask
 pip install flask-login
 pip install flask-sqlalchemy
 ```
+
+## Run
+
+Start the server
+
+```bash
+python main.py
+```
+
+And go to [http://127.0.0.1:5000`](http://127.0.0.1:5000`)
--- flask-demo/website/views.py
+++ flask-demo/website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint("views", __name__)
 
@@ -11,7 +12,6 @@ views = Blueprint("views", __name__)
 def home():
     if request.method == "POST":
         note = request.form.get("note")
-
         if len(note) < 1:
             flash("Note is too short!", category="error")
         else:
@@ -20,3 +20,15 @@ def home():
             db.session.commit()
             flash("Note added", category="success")
     return render_template("home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note["noteId"]
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
