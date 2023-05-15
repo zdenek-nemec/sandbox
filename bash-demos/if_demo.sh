f79a5b4b4c96f3cb6a3fd1892fd8de5bdd64e6d4
zdenek-nemec <zdenek.nemec@artin.cz>
2023-05-15 19:48:11 +0200

    #31: Else-if

--- bash-demos/if_demo.sh
+++ bash-demos/if_demo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-a=3
-if [[ $a == 1 || $a == 2 ]]; then
-    echo "Variable a is 1 or 2"
+a=4
+if [[ $a == 1 || $a == 2 || $a == 3 ]]; then
+    echo "Variable a is 1, 2 or 3"
+elif [[ $a == 4 ]]; then
+    echo "Variable a is 4"
 else
-    echo "Variable a is not 1 nor 2"
+    echo "Variable a is not 1, 2, 3 nor 4"
 fi
