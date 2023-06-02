a1deac79a49f72d813f6f0ea4c54336cf95ffa14
Nemec,Zdenek [External] <Zdenek.Nemec@external.drei.com>
2023-06-02 21:50:05 +0200

    #102: Include full-path into key

--- ics-sv-res-analysis-python/main.py
+++ ics-sv-res-analysis-python/main.py
@@ -9,7 +9,7 @@ class Report(object):
     def __init__(self):
         self._data = {}
 
-    def add(self, key: str, duration: int):
+    def add(self, key: tuple, duration: int):
         if key not in self._data:
             self._data[key] = duration
         else:
@@ -18,11 +18,12 @@ class Report(object):
     def size(self):
         return len(self._data)
 
-    def save(self, path: str, key: str = "Key"):
+    def save(self, path: str, key_column: str = "Key"):
         with open(path, "w", newline="") as csv_file:
             writer = csv.writer(csv_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([key, "Duration", "Duration Converted"])
+            writer.writerow([key_column, "Full-Path", "Duration", "Duration Converted"])
             for key in sorted(self._data.keys()):
+                core_key, full_path = key
                 duration = self._data[key]
                 hours = duration // (60 * 60)
                 minutes = (duration % (60 * 60)) // 60
@@ -30,7 +31,7 @@ class Report(object):
                 if duration != hours * 60 * 60 + minutes * 60 + seconds:
                     raise RuntimeError("Duration mismatch")
                 duration_converted = f"{hours}:{minutes:0>2}:{seconds:0>2}"
-                writer.writerow([key, duration, duration_converted])
+                writer.writerow([core_key, full_path, duration, duration_converted])
 
 
 def process_sv_export(sv_export_file_path: str, reports_directory_path: str):
@@ -43,10 +44,11 @@ def process_sv_export(sv_export_file_path: str, reports_directory_path: str):
             if i == 0 and row[0] == "TRACE":
                 continue
             a_party = row[18]
+            full_path = row[41]
             charge_start_date = row[44][6:10] + "-" + row[44][3:5] + "-" + row[44][0:2]
             duration = int(row[53])
-            a_party_report.add(a_party, duration)
-            date_report.add(charge_start_date, duration)
+            a_party_report.add((a_party, full_path), duration)
+            date_report.add((charge_start_date, full_path), duration)
 
     print(f"{a_party_report.size()=}")
     print(f"{date_report.size()=}")
