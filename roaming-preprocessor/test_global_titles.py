0f94699a14cbaa46082b3530c89c50496023707f
Nemec,Zdenek [External] <Zdenek.Nemec@external.drei.com>
2023-06-28 12:16:13 +0200

    #95: Updated GlobalTitles unit tests

--- roaming-preprocessor/test_global_titles.py
+++ roaming-preprocessor/test_global_titles.py
@@ -9,32 +9,32 @@ class TestGlobalTitles(unittest.TestCase):
         if socket.gethostname()[0:7] == "celimdt":
             self.global_titles = GlobalTitles("/appl/cgi/dcs/data01_loc/TOOLS/roaming-preprocessor/global_titles.csv")
         else:
-            self.global_titles = GlobalTitles("c:/Zdenek/_tmp/Cetin/roaming-preprocessor/global_titles.csv")
+            self.global_titles = GlobalTitles("o:/Cetin/roaming-processing/global_titles.csv")
 
     def test_exact_match(self):
         actual_output = self.global_titles.get_tadig("8835150")
-        expected_output = "AAAWL"
+        expected_output = ('8835150', 'AAAWL')
         self.assertEqual(expected_output, actual_output)
 
     def test_longer(self):
         actual_output = self.global_titles.get_tadig("8835150123")
-        expected_output = "AAAWL"
+        expected_output = ('8835150', 'AAAWL')
         self.assertEqual(expected_output, actual_output)
 
     def test_shortest(self):
         actual_output = self.global_titles.get_tadig("56")
-        expected_output = "CHLVT"
+        expected_output = ('56', 'CHLVT')
         self.assertEqual(expected_output, actual_output)
 
     def test_missing(self):
-        actual_output = self.global_titles.get_tadig("123")
-        expected_output = None
+        actual_output = self.global_titles.get_tadig("456")
+        expected_output = ("456", None)
         self.assertEqual(expected_output, actual_output)
 
     def test_missing_with_default(self):
-        for default_value in ["123", "miss"]:
-            actual_output = self.global_titles.get_tadig("123", default_value)
-            expected_output = default_value
+        for default_value in ["456", "miss"]:
+            actual_output = self.global_titles.get_tadig("789", default_value)
+            expected_output = ("789", default_value)
             self.assertEqual(expected_output, actual_output)
 
 
