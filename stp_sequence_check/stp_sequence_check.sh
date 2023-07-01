bfdc6ff0a5406abe1deab29760acee990c403b15
zdenek-nemec <zdenek.nemec@artin.cz>
2023-07-01 13:33:19 +0200

    #104 Refactoring and new tests

--- stp_sequence_check/stp_sequence_check.sh
+++ stp_sequence_check/stp_sequence_check.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 #
-# Name: check_stp_sequence.sh
+# Name: stp_sequence_check.sh
 #
 # Synopsis:
-#     check_stp_sequence.sh [ALTERNATIVE_PATH]
+#     stp_sequence_check.sh [-h] [-p PATH]
 #
 # Examples:
-#     check_stp_sequence.sh
-#     check_stp_sequence.sh /appl/cgi/dcs/data01_loc/tmp/x0562300/check_stp_sequence/testing/tc01_match
+#     stp_sequence_check.sh -h
+#     stp_sequence_check.sh
+#     stp_sequence_check.sh -p ./testing/tc01
 #
 # Description:
 #     Sequence number check in STP archive from today and yesterday. Default
-#     archive path is /cdr_archive/INPUT/A_RR/RR.
+#     path to archive is /cdr_archive/INPUT/A_RR/RR.
 #
-#     ALTERNATIVE_PATH
-#         Alternative archive path to use instead of default
+#     -h, --help
+#         Show this help message and exit
+#     -p PATH, --path PATH
+#         Use provided path to archive instead of the default one
 #
 
+DEFAULT_ARCHIVE_PATH=/cdr_archive/INPUT/A_RR/RR
+
 function print_usage {
-    echo "Usage: $(basename $BASH_SOURCE) [ALTERNATIVE_PATH]"
-    echo "Try \`$(basename $BASH_SOURCE) --help' for more information."
+    echo "Usage: ${BASH_SOURCE##*/} [-h] [-p PATH]"
+    echo "Try \`${BASH_SOURCE##*/} --help' for more information."
 }
 
 function print_help {
     head -$(grep -n -m 1 -v "^#" "$BASH_SOURCE" | cut -d ":" -f 1) "$BASH_SOURCE"
 }
 
-if [[ "$#" == "1" && ("$1" == "-h" || "$1" == "--help") ]]; then
+if [[ $# == 1 && ($1 == "-h" || $1 == "--help") ]]; then
     print_help
     exit 0
-elif [[ "$#" == "1" ]]; then
-    rr_archive_path=$1
-elif [[ "$#" == "0" ]]; then
-    rr_archive_path=/cdr_archive/INPUT/A_RR/RR
+elif [[ $# == 0 ]]; then
+    archive_path=$DEFAULT_ARCHIVE_PATH
+elif [[ $# == 2 && ($1 == "-p" || $1 == "--path") ]]; then
+    archive_path=$2
 else
+    echo "ERROR: Invalid arguments"
     print_usage
+    exit -1
+fi
+
+if [ ! -d $archive_path ]; then
+    echo "ERROR: Invalid archive path $archive_path"
     exit -1
 fi
 
@@ -43,13 +54,14 @@ archive_date_yesterday=$(date -d "1 day ago" "+%Y%m%d")
 archive_date_today=$(date "+%Y%m%d")
 for source in STP_BO STP_PH; do
     echo -n "$source $archive_date_yesterday+$archive_date_today:"
-    seq_numbers=$(ls $rr_archive_path/$archive_date_yesterday $rr_archive_path/$archive_date_today | grep $source | cut -c 37-42)
+    seq_numbers=$(ls $archive_path/$archive_date_yesterday $archive_path/$archive_date_today | grep $source | cut -c 37-42)
     seq_numbers_sorted=$(echo "$seq_numbers" | sort | uniq)
     if [[ $seq_numbers ]]; then
         seq_start=$(echo "$seq_numbers" | head -1)
         seq_end=$(echo "$seq_numbers" | tail -1)
-        echo " checking sequence $seq_start--$seq_end"
+        echo " checking sequence $seq_start-$seq_end"
         diff --side-by-side --suppress-common-lines <(echo "$seq_numbers_sorted") <(seq  $seq_start $seq_end) | grep ">" | awk '{print "WARNING, missing sequence number: " $2}'
-    else echo " no data"
+    else
+        echo " no data"
     fi
 done
--- stp_sequence_check/test_stp_sequence_check.sh
+++ stp_sequence_check/test_stp_sequence_check.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
+#
+# Name: test_stp_sequence_check.sh
+#
+# Synopsis:
+#     test_stp_sequence_check.sh [-h]
+#
+# Examples:
+#     test_stp_sequence_check.sh -h
+#     test_stp_sequence_check.sh
+#
+# Description:
+#     Test stp_sequence_check.sh script.
+#
+#     -h, --help
+#         Show this help message and exit
+#
 
-echo "Hello, World!"
+function print_usage {
+    echo "Usage: ${BASH_SOURCE##*/} [-h]"
+    echo "Try \`${BASH_SOURCE##*/} --help' for more information."
+}
 
-parent_dir=${PWD##*/}
-parent_dir=${parent_dir:-/}
-echo "$parent_dir"
+function print_help {
+    head -$(grep -n -m 1 -v "^#" "$BASH_SOURCE" | cut -d ":" -f 1) "$BASH_SOURCE"
+}
 
-if [[ "$parent_dir" != "stp_sequence_check" ]]; then
-    echo "Wrong parent directory"
+if [[ $# == 1 && ($1 == "-h" || $1 == "--help") ]]; then
+    print_help
+    exit 0
+elif [[ ! $# == 0 ]]; then
+    echo "ERROR: Invalid arguments"
+    print_usage
     exit -1
-else
-    echo "ok"
+fi
+
+parent_directory=${PWD##*/}
+parent_directory=${parent_directory:-/}
+if [ $parent_directory != "stp_sequence_check" ]; then
+    echo "ERROR: This test can be run only from stp_sequence_check root directory"
+    exit -1
 fi
 
 if [[ -d "./testing" ]]; then
-    echo "test dir present, removing"
+    echo "Testing directory exists already, removing"
     rm -rf ./testing
-else
-    echo "test dir missing, ok"
 fi
 
 date_yesterday=$(date -d "1 day ago" "+%Y%m%d")
 date_today=$(date "+%Y%m%d")
 
-# 20230628_120202_STP_PH_202306280819-612164.gz
-# At least first sequence in the day and latest today's sequence must be correct
-
+echo "Testing started"
+exit 0
 echo "TC01 - Unbroken sequence"
 mkdir -p ./testing/tc01/$date_yesterday
 touch ./testing/tc01/$date_yesterday/${date_yesterday}_010000_STP_PH_${date_yesterday}hhmm-500001.gz
@@ -38,7 +63,7 @@ mkdir -p ./testing/tc01/$date_today
 touch ./testing/tc01/$date_today/${date_today}_070000_STP_PH_${date_today}hhmm-500007.gz
 touch ./testing/tc01/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-500008.gz
 touch ./testing/tc01/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-500009.gz
-ls -1 ./testing/tc01/*/*
+./stp_sequence_check.sh --path ./testing/tc01
 echo ""
 
 echo "TC02 - Broken sequence"
@@ -50,7 +75,7 @@ mkdir -p ./testing/tc02/$date_today
 touch ./testing/tc02/$date_today/${date_today}_070000_STP_PH_${date_today}hhmm-500007.gz
 touch ./testing/tc02/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-500008.gz
 touch ./testing/tc02/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-500009.gz
-ls -1 ./testing/tc02/*/*
+./stp_sequence_check.sh --path ./testing/tc02
 echo ""
 
 echo "TC03 - Broken sequence completed"
@@ -65,7 +90,7 @@ touch ./testing/tc03/$date_today/${date_today}_073004_STP_PH_${date_today}hhmm-5
 touch ./testing/tc03/$date_today/${date_today}_073005_STP_PH_${date_today}hhmm-500005.gz
 touch ./testing/tc03/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-500008.gz
 touch ./testing/tc03/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-500009.gz
-ls -1 ./testing/tc03/*/*
+./stp_sequence_check.sh --path ./testing/tc03
 echo ""
 
 echo "TC04 - Unbroken sequence and more"
@@ -82,7 +107,7 @@ mkdir -p ./testing/tc04/$date_today
 touch ./testing/tc04/$date_today/${date_today}_070000_STP_PH_${date_today}hhmm-500007.gz
 touch ./testing/tc04/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-500008.gz
 touch ./testing/tc04/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-500009.gz
-ls -1 ./testing/tc04/*/*
+./stp_sequence_check.sh --path ./testing/tc04
 echo ""
 
 echo "TC05 - Unbroken sequence with rollover"
@@ -97,5 +122,21 @@ mkdir -p ./testing/tc05/$date_today
 touch ./testing/tc05/$date_today/${date_today}_070000_STP_PH_${date_today}hhmm-000003.gz
 touch ./testing/tc05/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-000004.gz
 touch ./testing/tc05/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-000005.gz
-ls -1 ./testing/tc05/*/*
+./stp_sequence_check.sh --path ./testing/tc05
 echo ""
+
+echo "TC06 - Broken sequence with rollover"
+mkdir -p ./testing/tc06/$date_yesterday
+touch ./testing/tc06/$date_yesterday/${date_yesterday}_010000_STP_PH_${date_yesterday}hhmm-999997.gz
+touch ./testing/tc06/$date_yesterday/${date_yesterday}_030000_STP_PH_${date_yesterday}hhmm-999999.gz
+touch ./testing/tc06/$date_yesterday/${date_yesterday}_040000_STP_PH_${date_yesterday}hhmm-000000.gz
+touch ./testing/tc06/$date_yesterday/${date_yesterday}_050000_STP_PH_${date_yesterday}hhmm-000001.gz
+touch ./testing/tc06/$date_yesterday/${date_yesterday}_060000_STP_PH_${date_yesterday}hhmm-000002.gz
+mkdir -p ./testing/tc06/$date_today
+touch ./testing/tc06/$date_today/${date_today}_070000_STP_PH_${date_today}hhmm-000003.gz
+touch ./testing/tc06/$date_today/${date_today}_080000_STP_PH_${date_today}hhmm-000004.gz
+touch ./testing/tc06/$date_today/${date_today}_090000_STP_PH_${date_today}hhmm-000005.gz
+./stp_sequence_check.sh --path ./testing/tc06
+echo ""
+
+echo "Testing finished"
