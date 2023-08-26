e90b60968c17253f17a49417c2e8a3faa5f84973
zdenek-nemec <zdenek.nemec@artin.cz>
2023-08-26 16:11:01 +0200

    #105 Part 2: Collisions and Events

--- pygame/coders-legacy-tutorial-car/Game.py
+++ pygame/coders-legacy-tutorial-car/Game.py
@@ -1,24 +1,29 @@
+# Imports
 import pygame, sys
 from pygame.locals import *
-import random
+import random, time
 
+# Initializing
 pygame.init()
 
+# Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-# Predefined some colors
+# Creating colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Screen information
+# Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
+SPEED = 5
 
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
@@ -31,13 +36,10 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
     def move(self):
-        self.rect.move_ip(0, 10)
-        if (self.rect.bottom > 600):
+        self.rect.move_ip(0, SPEED)
+        if (self.rect.top > 600):
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
 
 class Player(pygame.sprite.Sprite):
@@ -47,7 +49,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (160, 520)
 
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
         # if pressed_keys[K_UP]:
         # self.rect.move_ip(0, -5)
@@ -61,24 +63,50 @@ class Player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
-
+# Setting up Sprites
 P1 = Player()
 E1 = Enemy()
 
+# Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game Loop
 while True:
+
+    # Cycles through all events occuring
     for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 2
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    P1.update()
-    E1.move()
 
     DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+
+    # Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+    # To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        DISPLAYSURF.fill(RED)
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
 
     pygame.display.update()
     FramePerSec.tick(FPS)
