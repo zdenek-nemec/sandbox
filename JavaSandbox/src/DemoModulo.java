04be0818b695c90eebfbbf4be0e9decb5bec2bfd
zdenek-nemec <zdenek.nemec@artin.cz>
2025-02-20 14:53:05 +0100

    #20 Java modulo demo

---
+++ JavaSandbox/src/DemoModulo.java
@@ -0,0 +1,10 @@
+public class DemoModulo {
+    public static void main(String[] args) {
+        System.out.println("Demo: Modulo");
+
+        System.out.println("5 % 3 = " + 5 % 3);
+        System.out.println("-5 % 3 = " + -5 % 3);
+        System.out.println("5 % -3 = " + 5 % -3);
+        System.out.println("-5 % -3 = " + -5 % -3);
+    }
+}
