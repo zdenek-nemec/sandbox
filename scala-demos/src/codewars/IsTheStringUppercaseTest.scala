1ebf1bea04fba21d4d54bf95badf4cb7af2bce8c
zdenek-nemec <zdenek.nemec@artin.cz>
2025-03-07 16:29:36 +0100

    #110: Scala: Is the string uppercase?

--- scala-demos/src/codewars/IsTheStringUppercaseTest.scala
+++ scala-demos/src/codewars/IsTheStringUppercaseTest.scala
@@ -17,3 +17,33 @@ class IsTheStringUppercaseTest {
     assertEquals(true, IsTheStringUppercase.isUpperCase2("AHOJ"))
   }
 }
+
+
+class Point {
+  private var _x = 0
+  private var _y = 0
+  private val bound = 100
+
+  def x: Int = _x
+  def x_=(newValue: Int): Unit = {
+    if (newValue < bound)
+      _x = newValue
+    else
+      printWarning()
+  }
+
+  def y: Int = _y
+  def y_=(newValue: Int): Unit = {
+    if (newValue < bound)
+      _y = newValue
+    else
+      printWarning()
+  }
+
+  private def printWarning(): Unit =
+    println("WARNING: Out of bounds")
+}
+
+val point1 = new Point
+point1.x = 99
+point1.y = 101 // prints the warning
