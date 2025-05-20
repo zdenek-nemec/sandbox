872b06569c7a50fe475061d4849806593edc02e8
zdenek-nemec <zdenek.nemec@artin.cz>
2025-05-20 13:36:18 +0200

    #119 Barcode generator

--- barcodes/main.py
+++ barcodes/main.py
@@ -26,9 +26,15 @@ def read_barcode(image_path: str):
                           (255, 0, 0), 2)
             if barcode.data:
                 print(f"{image_path}: {barcode.data=}, {barcode.type=}")
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow("Image", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+def detect_barcodes(directory="img"):
+    for image_filename in os.listdir(directory):
+        image_path = f"{directory}/{image_filename}"
+        read_barcode(image_path)
 
 
 def main():
@@ -42,11 +48,14 @@ def main():
     # ]:
     #     read_barcode(image_path)
 
-    for printer in os.listdir("extracted_images"):
-        for image_filename in os.listdir(f"extracted_images/{printer}"):
-            image_path = f"extracted_images/{printer}/{image_filename}"
-            read_barcode(image_path)
-        break
+    # for printer in os.listdir("extracted_images"):
+    #     for image_filename in os.listdir(f"extracted_images/{printer}"):
+    #         image_path = f"extracted_images/{printer}/{image_filename}"
+    #         read_barcode(image_path)
+
+    detect_barcodes("c:/Zdenek/_tmp/L4C-5060 QR/2025-05-20 MyScans")
+    # detect_barcodes("c:/Zdenek/_tmp/L4C-5060 QR/2025-05-20 Theirs")
+    # detect_barcodes("generated_qr")
 
 
 if __name__ == "__main__":
