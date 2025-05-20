a53e31a5749e4a9aae6265d29eab750b335e0855
ARTIN | Němec Zdeněk <Zdenek.Nemec@ysoft.com>
2025-05-20 21:33:15 +0200

    #133 Three pages with dark theme

--- wizard/main.py
+++ wizard/main.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
+def start():
+    return render_template('start.html')
+
+@app.route('/install')
+def install():
+    return render_template('install.html')
+
+@app.route('/finish')
+def finish():
+    return render_template('finish.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
