e4a156523d063340e1fe442d8b22067347e724c0
zdenek-nemec <zdenek.nemec@artin.cz>
2025-09-19 17:36:26 +0200

    #138 Ajax behaviour - cannot compile because of type

--- WicketScalaDemo/src/main/scala/ajaxcomponent/AjaxComponent.scala
+++ WicketScalaDemo/src/main/scala/ajaxcomponent/AjaxComponent.scala
@@ -1,12 +1,27 @@
 package org.zdenek
 package ajaxcomponent
 
+import org.apache.wicket.ajax.AjaxRequestTarget
+import org.apache.wicket.ajax.form.OnChangeAjaxBehavior
 import org.apache.wicket.markup.html.WebPage
-import org.apache.wicket.markup.html.basic.Label
 import org.apache.wicket.markup.html.form.TextField
 import org.apache.wicket.model.Model
 
-class AjaxComponent extends WebPage{
-  add(new TextField[String]("myInputField", Model.of("")))
-  add(new Label("myParagraph", "This is a paragraph from Scala code."))
+case class UserData(name: String, description: String)
+
+class AjaxComponent extends WebPage {
+  val userModel = Model.of(UserData("", ""))
+  val nameField = new TextField[String]("myDescriptionField", userModel.map(_.name))
+  val descriptionField = new TextField[String]("myNameField", userModel.map(_.description))
+
+//  Cannot compile because of type
+  nameField.add(new OnChangeAjaxBehavior {
+      override def onUpdate(target: AjaxRequestTarget): Unit = {
+        descriptionField.getDefaultModel.setObject(nameField.getModel.getObject)
+        target.add(descriptionField)
+      }
+    })
+
+  add(descriptionField)
+  add(nameField)
 }
